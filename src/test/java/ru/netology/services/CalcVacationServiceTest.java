0ab896d01eb16dab30b2a2987420dc7eba5742f7
Ksenya Kos <kskosyrevavv@gmail.com>
2026-05-06 10:52:19 +0300

    Parameterized tests

--- src/test/java/ru/netology/services/CalcVacationServiceTest.java
+++ src/test/java/ru/netology/services/CalcVacationServiceTest.java
@@ -1,23 +1,25 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcVacationServiceTest {
-    @Test
-    public void testIncome10_000Rub () {
+    @ParameterizedTest
+    @CsvFileSource (files = "src/test/resources/vocation.csv")
+    public void testIncome10_000Rub (int expected, int income, int expenses, int threshold) {
         CalcVacationService service = new CalcVacationService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+        int actual = service.calculate(income,expenses,threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    public void testIncome100_000Rub () {
-        CalcVacationService service = new CalcVacationService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void testIncome100_000Rub () {
+//        CalcVacationService service = new CalcVacationService();
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
 
